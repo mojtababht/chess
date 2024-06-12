d049fff232430c56c4ca095c823e7a8e4265ff2e
mojtaba <mojtababht@gmail.com>
2024-06-12 17:02:12 +0330

    -

--- pieces/queen.py
+++ pieces/queen.py
@@ -1,4 +1,15 @@
-from .piece import Piece
+from board.board import Board
+from . import Piece, Rook, Bishop
+
 
 class Queen(Piece):
     symbol = 'q'
+
+    def valid_moves(self, board: Board) -> list:  #TODO: check "check status" after move
+        moves = []
+        # if board.is_in_check():
+        #     ...
+        # else:
+        #     ...
+
+        return moves
