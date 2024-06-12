84975a8445fa20d1a14d176d270786a3df53be3e
mojtaba <mojtababht@gmail.com>
2024-06-12 09:03:19 +0330

    added valid_moves for Pawn without considering check

--- pieces/pawn.py
+++ pieces/pawn.py
@@ -1,11 +1,48 @@
+from board.board import Board
 from .piece import Piece
 
 
 class Pawn(Piece):
     symbol = 'p'
+    initial = True
+    en_passant = False
 
 
+    def valid_moves(self, board: Board) -> list:  #TODO: check "check status" after move
+        moves = []
+        x = self.cord[0]
+        y = self.cord[1]
+        if board.is_in_check():
+            ...
+        else:
+            square = board.get_square((x, y + 1))
+            if not square.piece:
+                moves.append(square)
+            if self.initial:
+                square = board.get_square((x, y + 2))
+                if not square.piece:
+                    moves.append(square)
 
+            moves.append(self.get_attack_moves(board))
+        return moves
+
+    def get_attack_moves(self, board: Board) -> list:
+        moves = []
+        # diagonal attack
+        square = board.get_square((self.cord[0] + 1, self.cord[1] + 1))
+        if square.piece and square.piece.color != self.color:
+            moves.append(square)
+        square = board.get_square((self.cord[0] - 1, self.cord[1] + 1))
+        if square.piece and square.piece.color != self.color:
+            moves.append(square)
+        # en passant
+        square = board.get_square((self.cord[0] + 1, self.cord[1]))
+        if square.piece and square.piece.color != self.color and square.piece.symbol == 'p' and square.piece.en_passant:
+            moves.append(square)
+        square = board.get_square((self.cord[0] - 1, self.cord[1]))
+        if square.piece and square.piece.color != self.color and square.piece.symbol == 'p' and square.piece.en_passant:
+            moves.append(square)
+        return moves
 
 
 # TODO: this is a sample of async await for promotion
