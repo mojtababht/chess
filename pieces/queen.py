bf670f8fa11aaa22a9ca4dd84ec553f52d35a453
mojtaba <mojtababht@gmail.com>
2024-06-15 07:54:55 +0330

    added valid_moves for Queen it is not completed

--- pieces/queen.py
+++ pieces/queen.py
@@ -5,11 +5,12 @@ from . import Piece, Rook, Bishop
 class Queen(Piece):
     symbol = 'q'
 
-    def valid_moves(self, board: Board) -> list:  #TODO: check "check status" after move
+    def valid_moves(self, board: Board) -> list:  # TODO: check "check status" after move
         moves = []
-        # if board.is_in_check():
-        #     ...
-        # else:
-        #     ...
+        if board.is_in_check():
+            ...
+        else:
+            moves.extend(Rook(self.color, self.cord).valid_moves(board))
+            moves.extend(Bishop(self.color, self.cord).valid_moves(board))
 
         return moves
