74a71081fa3280a894108a80f9e3ab22b0a7e18b
mojtaba <mojtababht@gmail.com>
2024-06-18 09:15:30 +0330

    added move method for King

--- pieces/king.py
+++ pieces/king.py
@@ -1,11 +1,13 @@
 from itertools import product
 
 from board.board import Board
+from board.square import Square
 from .piece import Piece
 
 
 class King(Piece):
     symbol = 'K'
+    moved = False
 
     def possible_moves(self, board: Board) -> list:
         moves = []
@@ -16,8 +18,41 @@ class King(Piece):
         for cord in product(possible_x, possible_y):  # combination of possible_x and possible_y
             square = board.get_square(cord)
             if square.piece:
-                if not square.piece.color == self.color:
-                    moves.append(square)
-                continue
+                if square.piece.color == self.color:
+                    continue
             moves.append(square)
+        moves.extend(self.get_castle_moves(board))
         return moves
+
+    def get_castle_moves(self, board:Board) -> list:
+        moves = []
+        if not self.moved:
+            for cord in [(i, self.cord[1]) for i in range(1, self.cord[0])]:
+                if board.get_square(cord).piece:
+                    break
+            else:
+                rook_square = board.get_square((0, self.cord[1]))
+                if piece := rook_square.piece:
+                    if piece.symbol == 'r':
+                        if not piece.moved:
+                            moves.append(board.get_square((1, self.cord[1])))
+            for cord in [(i, self.cord[1]) for i in range(self.cord[0] + 1, 7)]:
+                if board.get_square(cord).piece:
+                    break
+            else:
+                rook_square = board.get_square((7, self.cord[1]))
+                if piece := rook_square.piece:
+                    if piece.symbol == 'r':
+                        if not piece.moved:
+                            moves.append(board.get_square((1, self.cord[1])))
+        return moves
+
+    def move(self, board: Board, target: Square) -> bool:
+        self.cord = target.cord
+        if not self.moved:
+            if self.cord[1] - target.cord[1] == 2:
+                board.get_square((0, self.cord[1])).piece.move(board, board.get_square((2, self.cord[1])))
+            if self.cord[1] - target.cord[1] == -2:
+                board.get_square((7, self.cord[1])).piece.move(board, board.get_square((4, self.cord[1])))
+        self.moved = True
+        return True
