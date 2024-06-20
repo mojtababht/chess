a16ceeac85c5036086a7324d268dd4247d338d87
mojtaba <mojtababht@gmail.com>
2024-06-20 13:05:11 +0330

    not complete

--- main.py
+++ main.py
@@ -22,11 +22,35 @@ class Game:
         pygame.display.update()
 
     def handel_click(self, event):
-        if self.board.selected_piece:
-            ...
+        cord = (event.pos[0] // self.square_size, event.pos[1] // self.square_size)
+        square = self.board.get_square(cord)
+        if selected_piece := self.board.selected_piece:
+            if square in selected_piece.valid_moves(self.board):
+                ...
+            else:
+                selected_piece.selected = False
+                for old_move in selected_piece.valid_moves(self.board):
+                    color = (240, 217, 181) if square.color == 'white' else (181, 136, 99)
+                    rect = self.draw_rect(old_move.cord, color)
+                    if old_move.piece:
+                        self.blit_image(old_move.piece, rect)
+                        if square.piece.color == self.board.turn:
+                            square.selected = True
+                            self.board.selected_piece = square.piece
+                            rect = self.draw_rect(square.cord, (3, 255, 0))
+                            self.blit_image(square.piece, rect)
+                            for move in square.piece.valid_moves(self.board):
+                                if move.piece:
+                                    color = (236, 122, 96)
+                                    rect = self.draw_rect(move.cord, color)
+                                    self.blit_image(move.piece, rect)
+                                else:
+                                    if move.color == 'black':
+                                        color = (182, 198, 65)
+                                    else:
+                                        color = (200, 214, 80)
+                                    self.draw_rect(move.cord, color)
         else:
-            cord = (event.pos[0] // self.square_size, event.pos[1] // self.square_size)
-            square = self.board.get_square(cord)
             if square.piece and square.piece.color == self.board.turn:
                 square.selected = True
                 self.board.selected_piece = square.piece
